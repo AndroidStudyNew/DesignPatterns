54a735f5497ee1fe0d806ae2e97002f94131159c
Charles Zhu <644912187@qq.com>
2016-04-15 14:35:04 +0800

    桥接模式

---
+++ src/com/sjtu/pattern/structural/bridge/SourceSub2.java
@@ -0,0 +1,10 @@
+package com.sjtu.pattern.structural.bridge;
+
+public class SourceSub2 implements Sourceable {  
+  
+    @Override  
+    public void method() {  
+        System.out.println("this is the second sub!");  
+    }  
+
+}
---
+++ src/com/sjtu/pattern/structural/bridge/Sourceable.java
@@ -0,0 +1,5 @@
+package com.sjtu.pattern.structural.bridge;
+
+public interface Sourceable {
+	 public void method(); 
+}
